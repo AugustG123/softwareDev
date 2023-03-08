3fc267a28670614c293cbd97d5ea7c094a5ca51f
pineapplles <127068533+pineapplles@users.noreply.github.com>
2023-03-07 22:25:48 -0500

    Update logisitcRegression.py

--- logisitcRegression.py
+++ logisitcRegression.py
@@ -1,4 +1,18 @@
 import torch
 import numpy as np
+import openpyxl as xl
 
 print("hello")
+all_data
+
+def imported(filename):
+    wb = xl.open_workbook(filename)
+    sheet = wb.active
+
+    for row in range(1,sheet.max_row+1):
+        for data in range(2, sheet.max_col+1):
+            all_data=np.empty(sheet.max_row, sheet.max_col)
+            all_data[row][col] = sheet.cell(row,col)
+
+imported("pvalue_test2.xlsx")
+print(all_data)
