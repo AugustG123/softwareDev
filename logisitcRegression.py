fe99999585e3727be3921d46f7879f8b474e4af5
pineapplles <127068533+pineapplles@users.noreply.github.com>
2023-03-21 22:11:39 -0400

    Update logisitcRegression.py

--- logisitcRegression.py
+++ logisitcRegression.py
@@ -14,5 +14,31 @@ def imported(filename):
             all_data=np.empty(sheet.max_row, sheet.max_col)
             all_data[row][col] = sheet.cell(row,col)
 
+            
+def gradient_descent(number_of_bounds, num_iterations=200, learning_rate=.1):
+    final_values = numpy.ones(number_of_bounds) #creates array of 1's
+    for i in range(num_iterations):
+        final_values = arrayAddition(final_values, arrayMultiplication(learning_rate*-1,gradient(final_values)))
+        #subtracts a fixed portion of the gradient based on the current values
+        #need to create a graident() function
+    return final_values
+
+
+def arrayMultiplication(constant, array):
+    for value in array:
+        value*=constant
+    return array
+
+
+#array1 and array2 need to have the same size
+def arrayAddition(array1, array2):
+    array_final
+    for i in range(len(array1)):
+        array_final.append(array1[i] + array2[i])
+    return array_final
+        
+def gradient(current_values_array):
+    #tb made later
+            
 imported("pvalue_test2.xlsx")
 print(all_data)
